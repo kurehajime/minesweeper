e0ea013c375aa58d224aba82a404e7c0b3dbb9be
kurehajime <xiidec@gmail.com>
2022-08-12 15:10:15 +0900

    モデルの方に生やす

--- src/model/State.ts
+++ src/model/State.ts
@@ -1,6 +1,22 @@
-export type Cell = {
-    Open: boolean
-    Flag: boolean
-    Bomb: boolean
-    Count: number
+export class Cell {
+    public Open: boolean
+    public Flag: boolean
+    public Bomb: boolean
+    public Count: number
+
+    public constructor(Open: boolean, Flag: boolean, Bomb: boolean, Count: number) {
+        this.Open = Open
+        this.Flag = Flag
+        this.Bomb = Bomb
+        this.Count = Count
+    }
+
+    public static Copy(state: Cell): Cell {
+        return {
+            Open: state.Open,
+            Flag: state.Flag,
+            Bomb: state.Bomb,
+            Count: state.Count,
+        }
+    }
 }
