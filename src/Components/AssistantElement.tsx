543ece885ce2af0b866a37969ff76e5964ee3a7b
kurehajime <xiidec@gmail.com>
2022-08-13 14:35:51 +0900

    多言語化

--- src/Components/AssistantElement.tsx
+++ src/Components/AssistantElement.tsx
@@ -1,24 +1,29 @@
-import imgUrl from '../assets/dolphin.png';
+import dolphin from '../assets/dolphin.png';
+import clip from '../assets/clip.png';
 import './AssistantElement.css'
+import { useTranslation } from 'react-i18next'
 type Props = {
     show: boolean
     setLevel: (level: number) => void
 }
 
 export default function AssistantElement(props: Props) {
+    const { t } = useTranslation()
+
+    const lang = (navigator.language).toLowerCase().includes("ja") ? "ja" : "en";
     return (
         props.show ?
             <div className='assistant zoomIn' >
                 <div className="balloon"><p>
-                    <p><b>何かお困りですか？</b></p>
+                    <p><b>{t('can_i_help_you')}</b></p>
                     <ul>
-                        <li onClick={() => props.setLevel(1)}>標準難易度で遊びたい</li>
-                        <li onClick={() => props.setLevel(2)}>高難易度で遊びたい</li>
-                        <li onClick={() => props.setLevel(3)}>超難易度で遊びたい</li>
-                        <li onClick={() => props.setLevel(99)}>お前を消す方法</li>
+                        <li onClick={() => props.setLevel(1)}>{t('level_1')}</li>
+                        <li onClick={() => props.setLevel(2)}>{t('level_2')}</li>
+                        <li onClick={() => props.setLevel(3)}>{t('level_3')}</li>
+                        {lang ? <li onClick={() => props.setLevel(99)}>{t('level_99')}</li> : <></>}
                     </ul>
                 </p></div>
                 <div className='horn'>▼</div>
-                <img src={imgUrl} alt="assistant" />
+                <img src={lang == 'ja' ? dolphin : clip} alt="assistant" />
             </div > : <></>)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import GameElement from './Components/GameElement'
 import './index.css'
+import './i18n/configs'
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
